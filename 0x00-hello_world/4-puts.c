4eea1669eeb64d5505d28be08e41bc12eb3bb42c
Justice-bott <onyemajustice22@gmail.com>
2023-06-16 12:14:28 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
