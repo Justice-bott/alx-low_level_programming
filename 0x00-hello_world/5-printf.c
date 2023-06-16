5b3753f6e831a336d2b5fab71535f4bfa9e95807
Justice-bott <onyemajustice22@gmail.com>
2023-06-16 12:29:41 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
