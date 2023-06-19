d9cc8612acf8e36d34f529d89185894b741aad37
Justice-bott <onyemajustice22@gmail.com>
2023-06-19 12:13:16 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - main function
- *
- * Return: 1
+ * main - A program that print a line to standard error
+ * Return: 1(Success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
