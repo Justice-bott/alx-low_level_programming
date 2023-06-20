54a7fb408c2e8c6d0d73e785dcda1b6695cb1674
Justice-bott <onyemajustice22@gmail.com>
2023-06-20 06:06:40 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
