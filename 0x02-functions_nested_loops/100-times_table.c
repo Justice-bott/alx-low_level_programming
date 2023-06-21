86e84fa3d3224113453562f7a215c24ee5202f7e
Justice-bott <onyemajustice22@gmail.com>
2023-06-21 11:53:45 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,30 +14,30 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-				for (mult = 1; mult <= n; mult++)
-				{
-					_putchar(',');
-					_putchar(' ');
-					prod = num * mult;
-					if (prod <= 99)
-						_putchar(' ');
-							if (prod <= 9)
-								_putchar(' ');
-							if (prod >= 100)
-							{
-								_putchar((prod / 100) + '0');
-								_putchar(((prod / 10)) % 10 + '0');
-							}
-							else if (prod <= 99 && prod >= 10)
-							{
-								_putchar((prod / 10) + '0');
-							}
-							_putchar((prod % 10) + '0');
-				}
-			_putchar('\n');
-		}
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
 	}
 }
