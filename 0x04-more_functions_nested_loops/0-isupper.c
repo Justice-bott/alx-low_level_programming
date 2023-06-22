af3f51b641bc56353090be96e49e2245278b4923
Justice-bott <onyemajustice22@gmail.com>
2023-06-22 11:08:58 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,18 @@
 
 
 /**
- * _isupper -Check if a number is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: 0 or 1
  */
 
 
-int _isupper(int x)
+int _isupper(int c)
 
 {
 
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
