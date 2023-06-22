1204f0ca7fc3794814c343bec48304b37e616f91
Justice-bott <onyemajustice22@gmail.com>
2023-06-22 13:53:01 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 
- * Return: The numbers since 0 up to 
+ * print_numbers - Print the numbers since 0 up to
+ * Return: The numbers since 0 up to
  */
 
 void print_numbers(void)
-
 {
+
 	int x;
 
-	for (x =0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		_putchar(x + '0');
 	}
