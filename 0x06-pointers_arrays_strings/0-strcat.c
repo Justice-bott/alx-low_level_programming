7828dec285caa366b3bf0e1cd1a2f7741871f1b4
Justice-bott <onyemajustice22@gmail.com>
2023-06-29 10:07:51 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,11 @@
 char *_strcat(char *dest, char *src)
 
 {
+
 	int i;
-	
+
 	int j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -23,12 +24,10 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		
+
 		i++;
-		
 		j++;
 	}
-	
 	dest[i] = '\0';
 	return (dest);
 }
