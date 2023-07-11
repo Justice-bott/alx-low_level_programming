29f51d9cb8880f2efcfbac8dae035697cb16dc8a
Justice-bott <onyemajustice22@gmail.com>
2023-07-11 15:34:05 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
-			l++
+			l++;
 	}
 	l += ac;
 	str = malloc(sizeof(char) * l + 1);
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -41,5 +41,5 @@ int **alloc_grid(int width, int height)
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
-	return (mee)
+	return (mee);
 }
