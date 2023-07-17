4058d3482617ce4ae8786347cf6dc8099128fa9a
Justice-bott <onyemajustice22@gmail.com>
2023-07-17 15:13:54 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,21 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
-#include <stdlib.h>
-
-#include <stdio.h>
-
-#define ERR_MSG "Error"
-
 /**
-* is_digit - checks if a string contains a non-digit char
-* @s: string to be evaluated
-* Return: 0 if a non-digit is found, 1 otherwise
-*/
-
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
 int is_digit(char *s)
-
 {
-
 	int i = 0;
 
 	while (s[i])
@@ -28,14 +22,13 @@ int is_digit(char *s)
 }
 
 /**
-* _strlen - returns the length of a string
-* @s: string to evaluate
-* Return: the length of the string
-*/
-
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-
 	int i = 0;
 
 	while (s[i] != '\0')
@@ -46,27 +39,24 @@ int _strlen(char *s)
 }
 
 /**
-* errors - handles errors for main
-*/
-
+ * errors - handles errors for main
+ */
 void errors(void)
 {
-	printf(6"Error\n");
-
+	printf("Error\n");
 	exit(98);
 }
 
 /**
-* main - multiplies two positive numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: always 0 (Success)
-*/
-
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
@@ -76,7 +66,6 @@ int main(int argc, char *argv[])
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -85,7 +74,6 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
@@ -105,7 +93,7 @@ int main(int argc, char *argv[])
 	}
 	if (!a)
 		_putchar('0');
-_putchar('\n');
-free(result);
-return (0);
+	_putchar('\n');
+	free(result);
+	return (0);
 }
