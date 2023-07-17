abac0b8a4e8fe3a593f42496d3a99a4306b87168
Justice-bott <onyemajustice22@gmail.com>
2023-07-17 13:50:30 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,4 @@
 #ifndef DOG_H
-
 #define DOG_H
 
 /**
@@ -11,33 +10,22 @@
 */
 
 struct dog
-
 {
-
 	char *name;
-
 	float age;
-
 	char *owner;
-
 };
 
 /**
 * dog_t - typedef for struct dog
 */
-
-typedef struct dog dog_t;
+typedef struct dog  dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
 char *_strcpy(char *dest, char *src);
-
 int _strlen(char *s);
 
 #endif
